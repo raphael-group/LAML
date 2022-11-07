b0a228d413ac4fb85354f21322b75712708b089b
Gillian Chu <gc3045@node302.ionic.cs.princeton.edu>
2022-11-07 00:10:17 -0500

    code to plot ml_fels, and results file.

--- jobs_topology_search/plot.R
+++ jobs_topology_search/plot.R
@@ -1,5 +1,5 @@
 # setwd("/Users/uym2/my_gits/problin/MP_inconsistent")
-setwd("/n/fs/ragr-research/projects/problin/MP_inconsistent")
+setwd("/n/fs/ragr-research/projects/problin/jobs_topology_search")
 
 require(ggplot2)
 
@@ -8,4 +8,5 @@ d = read.table("results.txt",header=T)
 ggplot(d,aes(x=k,y=percent_correct*100,color=method)) + geom_line() + geom_point() +
   scale_x_log10() + theme_classic() + xlab("Number of sites") + ylab("Percent correct")
 
-ggsave("MP_inconsistent.pdf")
+# ggsave("MP_inconsistent.pdf")
+ggsave("ML_fels.pdf")
