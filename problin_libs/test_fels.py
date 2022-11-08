201f4ac06ae9e116cd496d9608f7df5e058f6e24
Gillian Chu <gc3045@node408.ionic.cs.princeton.edu>
2022-11-08 10:31:11 -0500

    Likelihood function test file updated.

--- problin_libs/test_fels.py
+++ problin_libs/test_fels.py
@@ -1,6 +1,6 @@
 # from math import log,exp
 from ml import wrapper_felsenstein as wf
-from ml3 import wrapper_felsenstein as wf2
+# from ml3 import wrapper_felsenstein as wf2
 from sequence_lib import read_sequences
 from distance_based_lib import ML_pairwise_estimate
 import numpy as np
@@ -20,12 +20,15 @@ T = "((a:0.0360971597765934,b:3.339535381892265)e:0.0360971597765934,(c:0.036097
 S = read_sequences("../MP_inconsistent/seqs_m10_k" + str(k) + ".txt")
 msa = S[0]
 
-# print("TEST 1")
+print("TEST 1")
 # print(wf(T, Q, msa, use_log=True, optimize_branchlengths=True, initials=20))
 # print(wf2(T, Q, msa, optimize_branchlengths=True, initials=20))
+
+print(wf(T, Q, msa, optimize_branchlengths=True, initials=20))
 
 
 print("TEST 2")
 T = "((a,b)e,(c,d)f)r;"
-print(wf(T, Q, msa, use_log=True, optimize_branchlengths=True, initials=20))
-print(wf2(T, Q, msa, optimize_branchlengths=True, initials=20))
+# print(wf(T, Q, msa, use_log=True, optimize_branchlengths=True, initials=20))
+# print(wf2(T, Q, msa, optimize_branchlengths=True, initials=20))
+print(wf(T, Q, msa, optimize_branchlengths=True, initials=20))
