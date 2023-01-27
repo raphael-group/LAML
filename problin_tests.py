48cfa32b95ccc6e58a4e14f702a9ddcbead516d5
uym2 <ceciliamaitouyen@gmail.com>
2023-01-27 14:25:59 -0500

    EM solver

--- problin_tests.py
+++ problin_tests.py
@@ -1,5 +1,6 @@
 from unit_tests.unit_tests_MLSolver import *
 from unit_tests.unit_tests_SpaLinSolver import *
+from unit_tests.unit_tests_EMSolver import *
 
 if __name__ == '__main__':
     unittest.main()
