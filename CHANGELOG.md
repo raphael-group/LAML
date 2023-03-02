0cc445893fce9b3dca4f1ce73d5e5fdb82991403
uym2 <ceciliamaitouyen@gmail.com>
2023-03-02 12:31:57 -0500

    add version, LICENSE, CHANGELOG, and rename the main script to run_problin.py

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+* Version 0.1:
+    * Preliminary version: Scipy and EM solvers to optimize branch lengths, nu, and phi given a tree topology.
--- problin_libs/__init__.py
+++ problin_libs/__init__.py
@@ -3,3 +3,11 @@
 min_llh = -1000 # the minimum log-likelihood value
 eps = 1e-10 # epsilon value, usually used as the lower bound for non-zero values
 conv_eps = 1e-8 # convergence threshold (to stop a search algorithm such as EM)
+
+PROGRAM_NAME = "Problin"
+PROGRAM_AUTHOR = ["Uyen Mai","Gillian Chu","Ben Raphael"]
+PROGRAM_LICENSE = "GNU General Public License, version 3"
+PROGRAM_VERSION = "0.1"
+PROGRAM_YEAR = "2023"
+PROGRAM_INSTITUTE = "Computer Science Department, Princeton University"
+PROGRAM_DESCRIPTION = "Probabilistic Lineage Tracing with Mixed-type Missing Data"
