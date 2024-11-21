406d21756a8121db9a20f9790ea7cd16a1ed349e
gillichu <gillichu@berkeley.edu>
2024-11-21 11:29:11 -0500

    typo: example renamed.

--- examples/example7/run_laml2.sh
+++ examples/example7/run_laml2.sh
@@ -1,1 +1,1 @@
-python ../../run_laml2.py -c example_small_alleletable.json -t starting.tree -p "uniform" -o LAML2_example8 --nInitials 1 --timescale 10 -v --readout_model "PMMC" 
+python ../../run_laml2.py -c example_small_alleletable.json -t starting.tree -p "uniform" -o LAML2_example7 --nInitials 1 --timescale 10 -v --readout_model "PMMC" 
