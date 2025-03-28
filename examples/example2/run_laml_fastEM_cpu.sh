7f765a354d9456096229b4e3be772c341e78e566
gillichu <gillichu@berkeley.edu>
2025-03-28 12:39:03 -0400

    Dropping spurious log file lines.

--- examples/example2/run_laml_fastEM_cpu.sh
+++ examples/example2/run_laml_fastEM_cpu.sh
@@ -1,1 +1,1 @@
-python ../../run_laml.py -c character_matrix.csv -t starting.tree -p priors.csv -o example2_fastEM_cpu --nInitials 1 --randomreps 1 --topology_search -v --solver "fastEM-cpu" --timescale 10
+python ../../run_laml.py -c character_matrix.csv -t starting.tree -p priors.csv -o example2_fastEM_cpu --nInitials 1 --randomreps 5 --topology_search -v --solver "fastEM-cpu" --timescale 10
